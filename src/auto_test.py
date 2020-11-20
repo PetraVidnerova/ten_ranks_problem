74fb301a07b1092a5378136ba98d5c19f0f988a0
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-11-20 23:25:30 +0000

    auto test using autokeras

--- src/auto_test.py
+++ src/auto_test.py
@@ -7,59 +7,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from model import create_model, MODELS
 from preload_data import DATA_DICT
+from tensorflow.keras.layers import Reshape 
+
+import autokeras as ak
 
 
-def test_model(name, X, y):
-    print("MODEL: ", name)
-    model, epochs = create_model(name, input_shape=(1,125), classes=2)
-    model.summary()
-    
-    model.compile(#loss="mse",
-        loss='binary_crossentropy',
-        #        optimizer=RMSprop(lr=0.000005, decay=1e-07),
-        optimizer=RMSprop(),
-        #                  optimizer=Adam(lr=0.00001),
-        metrics=['accuracy'])
- 
 
-    X = X.reshape(-1, 1, 125)
+def test_model(X, y):
+
+
+#    X = X.reshape(-1, 124)
     X_train, X_test, Y_train, Y_test = train_test_split(X, y,
                                                         stratify=y,
                                                         test_size=0.2,
                                                         random_state=42)
 
-
+    # model = ak.AutoModel(
+    #     inputs=[ak.Input()],
+    #     outputs=[ak.ClassificationHead()],
+    #     max_trials=100
+    # )
+    
+    model = ak.StructuredDataClassifier(max_trials=10, overwrite=True)
     model.fit(X_train, Y_train,
-              batch_size=516,
-              epochs=3000,
-              validation_data=(X_test, Y_test),
+              batch_size=2**10,
+              epochs=100,
               verbose=2)
-#              shuffle=True)
-
-    # for layer in model.layers:
-    #     print(f"{layer.name}: {layer.trainable}") 
-
-    for layer in model.layers:
-        if "dropout" not in layer.name:
-            layer.trainable = True
-
-    # for layer in model.layers:
-    #     print(f"{layer.name}: {layer.trainable}") 
-        
-    model.compile(#loss="mse",
-        loss='binary_crossentropy',
-        #        optimizer=RMSprop(lr=0.000005, decay=1e-07),
-        optimizer=RMSprop(),
-        #                  optimizer=Adam(lr=0.00001),
-        metrics=['accuracy'])
-
-        
-    model.fit(X_train, Y_train,
-              batch_size=2**16,
-              epochs=10000,
-              validation_data=(X_test, Y_test),
-              verbose=2)
-#              shuffle=True)
+    results = model.predict(X_test)
 
 
     _, train_acc = model.evaluate(X_train, Y_train, verbose=2)
@@ -84,12 +58,10 @@ if __name__ == "__main__":
     # X = np.load(f"{DATA_DICT}/X_3vs6.npy")
     # y = np.load(f"{DATA_DICT}/y_3vs6.npy")
 
-    X = np.load(f"{DATA_DICT}/X_2vs5.npy")
-    y = np.load(f"{DATA_DICT}/y_2vs5.npy")
+    X = np.load(f"{DATA_DICT}/X_0vs2.npy")
+    y = np.load(f"{DATA_DICT}/y_0vs2.npy")
 
-    import sys
-    model = int(sys.argv[1])
 
-    test_model(MODELS[model], X, y)
+    test_model(X, y)
     # for name in MODELS:
     #     test_model(name, X, y)
--- src/preload_data.py
+++ src/preload_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-DATA_DICT = "../data/tens_rank_8_1e4"
-MAX_CLASS = 8
+DATA_DICT = "../data/tens_rank_6_1e5"
+MAX_CLASS = 6
 N = 126
 
 
@@ -13,12 +13,13 @@ def load_data(valid_columns=None, label=None):
         valid_columns = list(range(1, MAX_CLASS+1))
 
     target = None
-    for i, class_number in enumerate(valid_columns):
-        df = pd.read_csv(f"{DATA_DICT}/tens_rank_0{class_number}.txt", sep=" ", header=None)
+    for i, class_name in enumerate(valid_columns):
+        df = pd.read_csv(f"{DATA_DICT}/tens_{class_name}.txt", sep=" ", header=None)
         if len(df.columns) != N:
             raise ValueError("inconsistent data")
         df[0] = i
         X.append(df.to_numpy())
+        print(i)
 
     X = np.concatenate(X)
     # shuffle
@@ -40,4 +41,5 @@ def load_data(valid_columns=None, label=None):
 
 
 if __name__ == "__main__":
-    load_data([3, 6], label="3vs6")
+    for i in range(1, MAX_CLASS+1):        
+        load_data(["unif", f"rank_0{i}"], label=f"0vs{i}")
